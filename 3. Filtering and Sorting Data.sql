b9afa9ca85dd9a4e1e36178f330794d56cb50a8d
Maryam Zohoori <73991253+MaryamZohouri@users.noreply.github.com>
2025-12-18 10:19:05 +0330

    Add files via upload

---
+++ 3. Filtering and Sorting Data.sql
@@ -0,0 +1,28 @@
+﻿--Filtering and ordering
+--Filtering is by WHERE Clause via SELECT
+--Filtering by WHERE
+
+use BAS
+
+select * from Products
+
+SELECT ProductName, Price
+FROM Products
+WHERE Price>100
+
+
+--Sorting data
+--It's via ORDER BY
+SELECT ProductName, Price
+FROM Products
+ORDER BY Price DESC, ProductName ASC;
+
+--Ex. create a table from your information. Then do some exercise about "Filter and Sort".
+
+--Combining Filtering and Sorting
+select*from Orders
+
+SELECT CustomerName, OrderDate, TotalAmount
+FROM Orders
+WHERE TotalAmount>150
+ORDER BY OrderDate ASC
